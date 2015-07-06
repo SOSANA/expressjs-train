674933a3853437121770614673bb9f7607d46467
sosana <thesosana@gmail.com>
2015-07-06 02:41:33 +0000

    creating basic hello world in express

--- expressjs-blueprints/ch1/server.js
+++ expressjs-blueprints/ch1/server.js
@@ -1,7 +1,12 @@
 /**
  * Notes Go Here
- * 
- * 
-*/
-
+ */
 var express = require('express');
+var app = express();
+
+app.get('/', function (req, res, next) {
+  res.send('hello world');
+});
+app.listen(3000, function () {
+  console.log("app listening on 3000");
+});
