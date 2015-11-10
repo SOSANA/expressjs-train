80494c77f9fb9cfa111aceb7143cc69809b4fc44
sosana <thesosana@gmail.com>
2015-11-10 11:28:20 -0700

    updating bears api for get, post, put, and delete

--- scotch-io_node-api/server.js
+++ scotch-io_node-api/server.js
@@ -66,6 +66,46 @@ router.route('/bears')
     });
 });
 
+// on routes that end in /bears/:bear_id
+router.route('/bears/:bear_id')
+
+  // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+  .get(function(req, res) {
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err)
+        res.send(err);
+      res.json(bear);
+    });
+  })
+  // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+  .put(function(req, res) {
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err)
+        res.send(err);
+
+      bear.name = req.body.name; // update the bears info
+
+      // save the bear
+      bear.save(function(err) {
+        if (err)
+          res.send(err);
+
+        res.json({ message: 'Bear updated!'});
+      });
+    });
+  })
+  // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+  .delete(function(req, res) {
+    Bear.remove({
+      _id: req.params.bear_id
+    }, function(err, bear) {
+      if (err)
+        res.send(err);
+
+      res.json({ message: 'Successfully deleted' });
+  });
+});
+
 // REGISTER OUR ROUTES
 // all our routes will be prefixed with /api
 app.use('/api', router);
