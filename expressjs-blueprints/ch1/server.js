1c238702c302728a749fb478daa88e2e431c4075
sosana <thesosana@gmail.com>
2015-11-18 18:34:42 -0700

    removing comments

--- expressjs-blueprints/ch1/server.js
+++ expressjs-blueprints/ch1/server.js
@@ -1,16 +1,6 @@
-/**
- * This file is the entry point for our application. It is here that we generate
- * an application, register routes, and finally listen for incoming requests on
- * port 3000 . The require('express') method returns a generator of applications
- */
 var express = require('express');
 var app = express();
 
-// setting the default view engine for express
-// using ejs render for .html extensions
-// specifying the folder that express should look into for view files
-// This is useful if you choose one templating option and later want to switch
-// to a new one in an incremental way
 app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 app.engine('html', require('ejs').__express);
