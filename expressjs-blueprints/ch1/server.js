74ba63d3edc6d77ea240e3c6ffa53b944803fefd
sosana <thesosana@gmail.com>
2015-11-18 18:21:10 -0700

    setting up view engines for jade and ejs

--- expressjs-blueprints/ch1/server.js
+++ expressjs-blueprints/ch1/server.js
@@ -1,11 +1,24 @@
 /**
- * Notes Go Here
+ * This file is the entry point for our application. It is here that we generate
+ * an application, register routes, and finally listen for incoming requests on
+ * port 3000 . The require('express') method returns a generator of applications
  */
 var express = require('express');
 var app = express();
 
+// setting the default view engine for express
+// using ejs render for .html extensions
+// specifying the folder that express should look into for view files
+// This is useful if you choose one templating option and later want to switch
+// to a new one in an incremental way
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.engine('html', require('ejs').__express);
+
+app.set('views', __dirname + '/views');
+
 app.get('/', function (req, res, next) {
-  res.send('hello world');
+  res.render('index');
 });
 app.listen(3000, function () {
   console.log("app listening on 3000");
