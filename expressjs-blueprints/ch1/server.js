697ebd232cf3887731e19a394c9b709f2bf82fd6
sosana <thesosana@gmail.com>
2015-11-20 18:06:54 -0700

    updating server to handle deprecated options

--- expressjs-blueprints/ch1/server.js
+++ expressjs-blueprints/ch1/server.js
@@ -1,15 +1,50 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var User = require('./models/user');
+var passport = require('./passport');
+
+mongoose.connect('mongodb://localhost/ebpch01', function (err) {
+  if (err) throw err;
+});
+
 var app = express();
 
 app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 app.engine('html', require('ejs').__express);
-
 app.set('views', __dirname + '/views');
 
-app.get('/', function (req, res, next) {
-  res.render('index');
+/**
+ * In order to use passport, we have to enable a few things for our server.
+ * First we need to enable cookies and session support. To enable session
+ * support, we add a cookie parser. This middleware parses a cookie object into
+ * 'req.cookies'. The session middleware lets us modify 'req.session' and have
+ * that data persist across requests. By default, it uses cookies, but it has a
+ * variety of session stores that you can configure. Then, we have to add
+ * 'body-parsing' middleware, which parses the body of HTTP requests into a
+ * JavaScript object 'req.body'. In our current case, we need this middleware to
+ * extract the e-mail and password fields from POST requests. Finally, we add
+ * the passport middleware and session support.
+ */
+app.use(cookieParser('my secret string'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(session({
+  secret: "my other secret string",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Express listening on port 3000');
 });
-app.listen(3000, function () {
-  console.log("app listening on 3000");
-});
+
+module.exports.app = app;
+var routes = require('./routes');
