b5338897fbd71d82ad26ea22a90cdbffc7a98b1c
sosana <thesosana@gmail.com>
2015-11-23 16:48:11 -0700

    updating routes

--- expressjs-blueprints/ch1/routes.js
+++ expressjs-blueprints/ch1/routes.js
@@ -6,18 +6,37 @@
  * place to come visit and see all of our defined routes. It also helps unclutter
  * our server.js file, which should be exclusively devoted to server configuration.
  */
+ var app = module.parent.exports.app;
 
-var app = module.parent.exports.app;
-var userRoutes = require('./controllers/user');
-var passport = require('./passport');
+ var userRoutes = require('./controllers/user');
+ var middleware = require('./controllers/middleware');
+ var passport = require('./passport');
 
-app.get('/', function(req, res, next) {
-  res.render('index');
-});
+ app.get('/', function(req, res, next) {
+   res.render('index');
+ });
+ app.get('/html', function(req, res, next) {
+   res.render('index.html');
+ });
+ app.get('/jade', function(req, res, next) {
+   res.render('index.jade');
+ });
 
-app.post('/users/login', userRoutes.postLogin);
-app.get('/users/register', userRoutes.showRegistrationForm);
-app.post('/users/register', userRoutes.createUser);
-app.get('/users/login', userRoutes.showLoginForm);
-app.post('/users/login', userRoutes.createSession);
-app.get('/users/:id', userRoutes.getProfile);
+ app.get('/users/register', userRoutes.getRegister);
+ app.post('/users/register', userRoutes.postRegister);
+ app.get('/users/login', userRoutes.getLogin);
+ app.post('/users/login', userRoutes.postLogin);
+ app.get('/users/:id', middleware.restrictedLoggedInUser, userRoutes.getProfile);
+
+ app.get('/auth/twitter', passport.authenticate('twitter'));
+ app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
+     res.redirect(req.session.returnTo || '/');
+ });
+ app.get('/auth/github', passport.authenticate('github'));
+ app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) {
+     res.redirect(req.session.returnTo || '/');
+ });
+ app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
+ app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
+     res.redirect(req.session.returnTo || '/');
+ });
