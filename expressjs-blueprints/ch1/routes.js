5681efbc0ea9b639d3cca630498dbfb39ec61708
sosana <thesosana@gmail.com>
2015-11-23 11:58:46 -0700

    updating our routes to session support

--- expressjs-blueprints/ch1/routes.js
+++ expressjs-blueprints/ch1/routes.js
@@ -19,3 +19,4 @@ app.post('/users/login', userRoutes.postLogin);
 app.get('/users/register', userRoutes.showRegistrationForm);
 app.post('/users/register', userRoutes.createUser);
 app.get('/users/login', userRoutes.showLoginForm);
+app.post('/users/login', userRoutes.createSession);
