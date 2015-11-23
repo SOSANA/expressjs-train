7e07d679c2d0b3f7bfae1ec9bfc3e04a8e5777ef
sosana <thesosana@gmail.com>
2015-11-23 16:32:03 -0700

    added route for user profile

--- expressjs-blueprints/ch1/routes.js
+++ expressjs-blueprints/ch1/routes.js
@@ -20,3 +20,4 @@ app.get('/users/register', userRoutes.showRegistrationForm);
 app.post('/users/register', userRoutes.createUser);
 app.get('/users/login', userRoutes.showLoginForm);
 app.post('/users/login', userRoutes.createSession);
+app.get('/users/:id', userRoutes.getProfile);
