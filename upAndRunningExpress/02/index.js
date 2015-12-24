d8a1b8095b81a9d2658ec94d139d36984832095f
sosana <thesosana@gmail.com>
2015-12-24 16:45:21 -0700

    updating app.js files with basic routes

--- upAndRunningExpress/02/index.js
+++ upAndRunningExpress/02/index.js
@@ -1,12 +1,46 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello, world!');
+var fs = require('fs');
+var _ = require('lodash');
+var users = [];
+
+fs.readFile('users.json', {encoding: 'utf8'}, function(err, data) {
+  if (err) throw err;
+
+  JSON.parse(data).forEach(function(user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
+    users.push(user);
+  });
 });
 
-app.get('/yo', function(req, res) {
-  res.send('Yo!');
+app.get('/', function(req, res) {
+  var buffer = '';
+
+  users.forEach(function(user) {
+    buffer += '<a href="/' + user.username + '">' + user.name.full + '</a><br>';
+  });
+
+  res.send(buffer);
+});
+
+// note that this has to be before the username params as it gets called once
+// use reg exp as the first argument to search for any strings that start with
+// big and any other characters, and log out to console
+app.get(/big.*/, function(req, res, next) {
+  console.log('BIG USER ACCESS');
+  next();
+});
+
+app.get(/.*dog.*/, function(req, res, next) {
+  console.log('DOGS GO WOOF WOOF');
+  next();
+});
+
+// able to access username by passing params
+app.get('/:username', function(req, res) {
+  var username = req.params.username;
+  res.send(username);
 });
 
 var server = app.listen(3000, function() {
